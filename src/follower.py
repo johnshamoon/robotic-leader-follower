a0b08c8292ecac1273ad70728a5c0dd0ee2353e6
Norma Chang <nchang15@gmail.com>
2019-03-26 23:07:44 -0400

    Added deadzones to camera and wheels

--- src/follower.py
+++ src/follower.py
@@ -52,9 +52,8 @@ class Follower:
 
         self.camera_angle_offset = 0
 
-        self.WHEEL_MIN = 45
-        self.WHEEL_MAX = 135
         self.STRAIGHT_ANGLE = 90
+        self.DEADZONE = 7
 
 
     """
@@ -93,18 +92,17 @@ class Follower:
         self._bw.forward()
 
 
-    """
+  """
     Turn the wheels and pans the camera towards the last recognized object.
     """
     def turn(self):
         self.convert_camera_angle()
         self.pan_camera()
 
-        if self.STRAIGHT_ANGLE < self._turn_angle:
-          self._fw.turn(self._turn_angle + self.camera_angle_offset) 
-
-        elif self.STRAIGHT_ANGLE > self._turn_angle:
-          self._fw.turn(self._turn_angle - self.camera_angle_offset)
+        if self.STRAIGHT_ANGLE - self.DEADZONE < self._turn_angle:
+            self._fw.turn(self._turn_angle + self.camera_angle_offset) 
+        elif self.STRAIGHT_ANGLE + self.DEADZONE > self._turn_angle:
+            self._fw.turn(self._turn_angle - self.camera_angle_offset)
 
     
     """
@@ -112,25 +110,14 @@ class Follower:
     """
     def pan_camera(self):
 
-        if self.WHEEL_MIN < self._turn_angle < self.STRAIGHT_ANGLE:
+        if self._turn_angle < self.STRAIGHT_ANGLE - self.DEADZONE:
             self.turn_camera_left(self._turn_angle)
-            self.step_to_angle()
-            self.camera_angle_offset = (self.STRAIGHT_ANGLE - self._turn_angle) 
+            self.camera_angle_offset = self._camera.current_pan - 90
 
-        elif self.WHEEL_MAX > self._turn_angle > self.STRAIGHT_ANGLE:
+        elif self._turn_angle > self.STRAIGHT_ANGLE + self.DEADZONE:
             self.turn_camera_right(self._turn_angle)
-            self.step_to_angle()
-            self.camera_angle_offset = (self._turn_angle - self.STRAIGHT_ANGLE) 
-
-
-    """
-    Converts steps into angles.
-    """
-    def step_to_angle(self):
-        self._turn_angle = self.angle_to_step()
-        self._turn_angle = self._turn_angle * self._camera.PAN_STEP + 1
-        return self._turn_angle
-        
+            self.camera_angle_offset = np.abs(self._camera.current_pan - 90)
+ 
 
     """
     Definitions for turning the camera left and right.
@@ -148,8 +135,7 @@ class Follower:
     Takes the angle property and converts into steps where 1 step is 5 degrees.
     """
     def angle_to_step(self):
-        steps = self._turn_angle/self._camera.PAN_STEP - 1
-        return steps
+        return (self._turn_angle/self._camera.PAN_STEP) - 1
 
 
     """
